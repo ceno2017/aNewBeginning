0292d0219ab75101fe488e74898000fe44edca45
Charles Akpan Eno <CEno@slb.com>
2021-05-27 15:37:06 +0100

    Worked on menu item,directory and styled same

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../components/directory/directory.component';
+
+const HomePage = ()=>(
+    <div classNam="homePage">
+         <Directory />
+    </div>
+)
+
+export default HomePage;
