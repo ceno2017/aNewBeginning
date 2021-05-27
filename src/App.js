f560952f6e33c46c522af77755e64803e449e276
Charles Akpan Eno <mybizdesk_50@yahoo.com>
2021-05-27 17:40:25 +0100

    used withRouter HOC on menu item
    worked with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Switch,Route} from 'react-router-dom';
 import HomePage from './pages/homepage.component';
 
-function App() {
+
+const HatsPage=()=>(
+  <div>
+    <h2>Hats Page</h2>
+  </div>
+);
+
+function App(props) {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
